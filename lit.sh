d969d7aa75e223e0ad09bf3e29f27f3f1b783af1
Sjors Ottjes <sjorsottjes@gmail.com>
2025-10-03 05:40:54 +0200

    wip

--- lit.sh
+++ lit.sh
@@ -13,7 +13,7 @@ command="$1"
 if [ "$command" = "pull" ]; then
     bash "$lit_base_path/scripts/pull.sh" "$lit_base_path"
 elif [ "$command" = "init" ]; then
-    bash "$lit_base_path/scripts/init.sh" "$lit_base_path"
+    bash "$lit_base_path/scripts/init.sh" "$lit_base_path" "$2"
 else
     cat "$lit_base_path/scripts/help.txt"
 fi
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,38 +2,66 @@
 
 set -e
 
-trap 'tput cnorm' EXIT INT TERM
-
-echo ""
-
-# if [ "$(ls -A . 2>/dev/null)" ]; then
-#     echo "Current directory is not empty."
-#     echo ""
-#     echo "Please try again in an empty directory."
-#     echo ""
-#     exit 1
-# fi
+if [ "$(ls -A . 2>/dev/null)" ]; then
+    echo "Current directory is not empty."
+    echo ""
+    echo "Please try again in an empty directory."
+    echo ""
+    exit 1
+fi
 
 lit_base_path="$1"
+git_repository_url="$2"
 project_base_path="$(pwd)"
 
 source "$lit_base_path/scripts/helpers.sh"
 
-# read -p "Enter a Git repository URL: " git_repository_url
-git_repository_url="git@github.com:Revenly/tratta-backend.git"
+if [ -z "$git_repository_url" ]; then
+    echo "usage: lit init <git_repository_url>"
+
+    exit 1
+fi
 
 mkdir -p "$project_base_path/lit"
 
 echo "$git_repository_url" > "$project_base_path/lit/git-repository-url"
 
-# tput civis
+printf "Reading \"$git_repository_url\"... "
 
-# git clone --depth 1 "$git_repository_url" initial-clone > /dev/null 2>&1 &
+default_branch_info=$(git ls-remote --symref "$git_repository_url" HEAD)
 
-# show_spinner $! "Cloning \"$git_repository_url\""
+default_branch=$(echo "$default_branch_info" | grep "ref: refs/heads/" | sed 's/ref: refs\/heads\///' | cut -f1)
+default_commit=$(echo "$default_branch_info" | grep -v "ref: refs/heads/" | cut -f1)
 
-git --git-dir="$project_base_path/initial-clone/.git" \
-    rev-parse --abbrev-ref HEAD > "$project_base_path/lit/current-branch"
+echo "$default_branch" > "$project_base_path/lit/current-branch"
+echo "$default_commit" > "$project_base_path/lit/current-commit"
 
-git --git-dir="$project_base_path/initial-clone/.git" \
-    rev-parse HEAD > "$project_base_path/lit/current-commit"
+echo "Done!"
+echo ""
+echo "Current branch set to \"$default_branch\""
+echo ""
+
+mkdir -p "$project_base_path/lit/hooks"
+cp "$lit_base_path/stubs/after-pulling-hook.sh.stub" "$project_base_path/lit/hooks/after-pulling.sh"
+cp "$lit_base_path/stubs/before-activitation-hook.sh.stub" "$project_base_path/lit/hooks/before-activitation.sh"
+cp "$lit_base_path/stubs/after-activation-hook.sh.stub" "$project_base_path/lit/hooks/after-activation.sh"
+
+touch "$project_base_path/.env"
+
+mkdir -p "$project_base_path/releases"
+
+mkdir -p "$project_base_path/storage/"{app/public,app/private,framework/{cache/data,sessions,testing,views},logs};
+
+echo "Finished initializing Lit in this directory"
+echo ""
+echo "Next steps:"
+echo "- Fill in the \".env\" file"
+echo "- Review these hooks and change them if necessary:"
+echo "  - \"lit/hooks/after-pulling.sh\""
+echo "  - \"lit/hooks/before-activitation.sh\""
+echo "  - \"lit/hooks/after-activation.sh\""
+echo ""
+echo "After that, either:"
+echo "- Run \"lit pull\" to deploy the current branch ($default_branch)"
+echo "- Run \"lit checkout <branch>\" to deploy a different branch"
+echo ""
