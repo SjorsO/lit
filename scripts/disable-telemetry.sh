707b95f69a4d902558e1c4dd1aee5218fdcfb2d6
Sjors Ottjes <sjorsottjes@gmail.com>
2025-10-04 17:12:18 +0200

    wip

--- scripts/disable-telemetry.sh
+++ scripts/disable-telemetry.sh
@@ -7,12 +7,13 @@ project_base_path="$2"
 
 source "$lit_base_path/scripts/helpers.sh"
 
-if [ ! -f "$lit_base_path/telemetry-enabled" ]; then
+if [ ! -f "$lit_base_path/data/telemetry-enabled" ]; then
     echo "Telemetry is already disabled"
     
     exit 1
 fi
 
-rm "$lit_base_path/telemetry-enabled"
+rm "$lit_base_path/data/telemetry-enabled"
 
-echo "Completely disabled telemetry for all projects"
+echo "Completely disabled telemetry"
+echo "No telemetry will be sent from this server (unless you run \"lit enable-telemetry\" again)"
--- scripts/enable-telemetry.sh
+++ scripts/enable-telemetry.sh
@@ -7,12 +7,12 @@ project_base_path="$2"
 
 source "$lit_base_path/scripts/helpers.sh"
 
-if [ -f "$lit_base_path/telemetry-enabled" ]; then
+if [ -f "$lit_base_path/data/telemetry-enabled" ]; then
     echo "Telemetry is already enabled"
     
     exit 1
 fi
 
-touch "$lit_base_path/telemetry-enabled"
+touch "$lit_base_path/data/telemetry-enabled"
 
 echo "Enabled anonymous telemetry"
