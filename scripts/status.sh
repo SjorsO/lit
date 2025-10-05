1666e18383ae1c68e5f7ebd1f5b46b2917865165
Sjors Ottjes <sjorsottjes@gmail.com>
2025-10-05 07:28:35 +0200

    wip

--- scripts/status.sh
+++ scripts/status.sh
@@ -12,15 +12,18 @@ current_branch="$(get_file_value "$project_base_path/lit/current-branch")"
 current_commit="$(get_file_value "$project_base_path/lit/current-commit")"
 
 reusing_status=$([ -f "$project_base_path/lit/reusing-enabled" ] && echo "enabled" || echo "disabled")
-telemetry_status=$([ -f "$lit_base_path/data/telemetry-enabled" ] && echo "enabled" || echo "disabled")
 
-line1=" Git repository url: $git_repository_url"
-line2="     Current branch: $current_branch"
-line3="     Current commit: $current_commit"
-line4="   Reusing releases: $reusing_status"
-line5="Anonymous telemetry: $telemetry_status"
+lines=()
+while IFS= read -r line; do
+    lines+=("$line")
+done <<EOF
+ Git repository url: $git_repository_url
+     Current branch: $current_branch
+     Current commit: $current_commit
+   Reusing releases: $reusing_status
+EOF
 
-for line in "$line1" "$line2" "$line3" "$line4" "$line5"; do
+for line in "${lines[@]}"; do
     if [ ${#line} -gt ${max_length:-0} ]; then
         max_length=${#line}
     fi
@@ -34,9 +37,9 @@ box_width=$((max_length + 2))
 horizontal_line=$(printf "─%.0s" $(seq 1 $box_width))
 
 echo "╭${horizontal_line}╮"
-printf "│ %-${max_length}s │\n" "$line1"
-printf "│ %-${max_length}s │\n" "$line2"
-printf "│ %-${max_length}s │\n" "$line3"
-printf "│ %-${max_length}s │\n" "$line4"
-printf "│ %-${max_length}s │\n" "$line5"
+
+for line in "${lines[@]}"; do
+    printf "│ %-${max_length}s │\n" "$line"
+done
+
 echo "╰${horizontal_line}╯"
