bf61d3b3e4005872326f85f6fe56d1e4cd0bc6ef
Sjors Ottjes <sjorsottjes@gmail.com>
2025-10-06 06:06:03 +0200

    Update lit.sh

--- lit.sh
+++ lit.sh
@@ -8,7 +8,7 @@ project_base_path="$(pwd)"
 command="$1"
 
 if [ ! -f "$lit_base_path/data/installation-id" ]; then
-    bash "$lit_base_path/scripts/install.sh" "$lit_base_path"
+    source "$lit_base_path/scripts/install.sh" "$lit_base_path"
 
     exit 0
 fi
